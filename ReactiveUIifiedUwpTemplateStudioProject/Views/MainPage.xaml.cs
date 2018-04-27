86a5707fce4c0311ca3d4ada4c6f9979e6d476e9
Colt Bauman <cabauman@maverickgames.co>
2018-04-28 08:50:46 +0900

    Add project files.

---
+++ ReactiveUIifiedUwpTemplateStudioProject/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Windows.UI.Xaml.Controls;
+
+using ReactiveUIifiedUwpTemplateStudioProject.ViewModels;
+
+namespace ReactiveUIifiedUwpTemplateStudioProject.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel
+        {
+            get { return DataContext as MainViewModel; }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
